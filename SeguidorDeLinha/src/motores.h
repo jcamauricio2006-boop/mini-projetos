d10c991b03eb555789f6ae62b7cb17442d0d131a
BomDia(GoodMorning) <jcamauricio2006@gmail.com>
2026-05-12 18:41:34 -0300

    Update motores.h

--- SeguidorDeLinha/src/motores.h
+++ SeguidorDeLinha/src/motores.h
@@ -5,10 +5,15 @@
 #define EN2  23
 #define EN3  25
 #define EN4  26
+int valor_pwm;
+int dif;
 
 void frente();
 void atras();
 void esquerda();
 void direita();
+void parar();
+void direitaT();
+void esquerdaT();
 
 #endif
